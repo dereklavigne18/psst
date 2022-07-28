e824aa365245e3afca95adf75c2b146a4e8eda30
Derek Lavigne <derek.lavigne@comcast.net>
2022-07-27 23:09:22 -0400

    Full cli up and running!

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM library/python:3.9.0
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install -r requirements.txt
--- psst.py
+++ psst.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import click
+
+
+@click.group()
+def cli_group():
+    pass
+
+
+@cli_group.command()
+def register():
+    print("Register!")
+
+
+@cli_group.command()
+def ask():
+    print("Ask!")
+
+
+if __name__ == '__main__':
+    cli_group()
